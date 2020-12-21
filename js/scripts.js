19a49deca44a4c09cab2b77a3863d6b09745b61f
chegrofelix <chegerofelix@gmail.com>
2020-12-21 23:24:30 +0300

    Added styles to css

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 //alert("hello")
+jQuery('h1').click(function(){
+    alert("This is a heading tag")
+  });
+
+  jQuery('p').click(function(){
+    alert("This is a paragraph")
+  });
+
+  jQuery('img').click(function(){
+    alert("This is an image")
+  });
