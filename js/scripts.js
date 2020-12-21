3efbc834c2dbfed90cde45e49d3a58b3c25028f9
chegrofelix <chegerofelix@gmail.com>
2020-12-22 00:33:04 +0300

    Changed contacts to contact us

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,9 @@ jQuery('h1').click(function(){
   jQuery('img').click(function(){
     alert("This is an image")
   });
+  //*$(document).ready(function() {
+    //$(".clickable").click(function() {
+      //$("#walrus-showing").toggle();
+      //$("#walrus-hidden").toggle();
+   // });
+  //});
